35b61f42d1c95f21b2a29803484bb63dcb43d28c
Vladislav Kruglikov <vladislavkruglikov@icloud.com>
2025-01-22 15:14:38 +0300

    Create

--- strawberry/user.py
+++ strawberry/user.py
@@ -2,48 +2,23 @@ import time
 import typing
 import asyncio
 
-from strawberry.dataset import Dataset
+from loguru import logger
 from strawberry.requester import Requester
+from strawberry.dataset import Sampler, OutputDataset
 
 
 class User:
-    """
-    Handles user interactions by managing request logic, waiting between requests, and sampling input data.
-
-    This class simulates a user that sends requests to a server using a provided requester instance.
-    The user logic includes programmable waiting times between requests and sampling prompts from a dataset.
-
-    Attributes:
-        _wait (typing.Callable): A callable function to determine wait time between requests.
-        _dataset (Dataset): The dataset from which input prompts are sampled.
-        _requester (Requester): The requester instance used to send requests.
-    """
-
-    def __init__(self, requester: Requester, wait: typing.Callable, dataset: Dataset) -> None:
-        """
-        Initializes the User instance.
-
-        Args:
-            requester (Requester): The requester instance to handle API requests.
-            wait (typing.Callable): A callable function that returns the wait time between requests in seconds.
-            dataset (Dataset): The dataset instance used to provide random prompts for requests.
-        """
+    def __init__(self, requester: Requester, wait: typing.Callable, dataset: Sampler, output_dataset_writer: OutputDataset) -> None:
         self._wait = wait
         self._dataset = dataset
         self._requester = requester
+        self._output_dataset_writer = output_dataset_writer
 
     async def start(self) -> None:
-        """
-        Starts the user simulation loop.
-
-        The user continuously sends requests using the requester instance with prompts sampled
-        from the dataset. Between requests, the user waits for a time determined by the `_wait` function.
-
-        The loop runs indefinitely.
-
-        Raises:
-            Exception: If an error occurs during a request or dataset sampling.
-        """
-        while True:
-            await self._requester.request(messages=[{"role": "user", "content": self._dataset.get_random_prompt()}])
-            await asyncio.sleep(self._wait())
+        async for request, is_last in self._dataset:
+            res = await self._requester.request(request)            
+            await self._output_dataset_writer.write_single_response(res)
+            if not is_last:
+                await asyncio.sleep(self._wait())
+        
+        logger.info("User finished processing")
